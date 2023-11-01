6234956d320ac7348661e9a15e084a0d09996c86
Aayushi Ghosh <137275145+electricalpigeon@users.noreply.github.com>
2023-11-02 01:45:31 +0530

    Update Arduino_LED_blink.ino

--- Arduino_LED_blink.ino
+++ Arduino_LED_blink.ino
@@ -1,4 +1,6 @@
 //Arduino MEGA-2560
+//Part-1
+//Board LED blink
 void setup() {
   DDRB=0x80; //PORTB7(pin 13) as output; DDR-Data direction register
   PORTB=0x00;
@@ -9,4 +11,20 @@ void loop() {
   delay(500);// no argument as fraction ;delay for 500 ms
   PORTB=0x00;//LED AT PIN 13 IS OFF
   delay(500);
+}//D7 bit has to be 1
+
+
+//Part-2
+//External LED blink
+void setup() {
+  DDRA=0x01; //PA0 as output pin
+  PORTA=0;
+}
+
+void loop() {
+  PORTA=0X01; //LED  ON
+  delay(250);// no argument as fraction ;delay for 250 ms
+  PORTA=0;//LED  OFF
+  delay(250);
+  
 }//D7 bit has to be 1
